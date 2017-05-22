6897701a6304c4d7326c85cc16e9009b71da72c1
mathankanagaraj <kmathan@outlook.com>
2017-05-22 17:16:06 +0530

    firt commit

--- environments/production/manifests/site.pp
+++ environments/production/manifests/site.pp
@@ -2,3 +2,23 @@ file { '/root/first.txt':
         ensure => present,
         content => "Hello this is test file..",
         }
+
+file { '/root/second.txt':
+        ensure => present,
+        content => "Uname is: $hostname \n",
+        }
+
+file { '/root/third.txt':
+        ensure => present,
+        content => "Harware:  ${facts['os']['hardware']}",
+        }
+
+node 'node1' {
+file { '/root/4.txt':
+        ensure => present,
+        content => "uptime is: ${facts['system_uptime']['uptime']}\n",
+        }
+}
+
+node default {
+}
